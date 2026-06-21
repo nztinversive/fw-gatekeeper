87abfc8c1ebef99cd35f7e4cb8c4821440672aaa
nztinversive <thiesnoah@hotmail.com>
2026-06-21 09:03:20 -0500

    Fix Convex client initialization during build

--- src/lib/convex.ts
+++ src/lib/convex.ts
@@ -1,4 +1,19 @@
 import { ConvexHttpClient } from "convex/browser";
 
-const client = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
-export default client;
+let client: ConvexHttpClient | null = null;
+
+export function getConvexClient() {
+  if (!client) {
+    client = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+  }
+
+  return client;
+}
+
+const convex = new Proxy({} as ConvexHttpClient, {
+  get(_target, prop, receiver) {
+    return Reflect.get(getConvexClient(), prop, receiver);
+  },
+});
+
+export default convex;
